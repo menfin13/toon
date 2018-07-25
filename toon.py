e832e567d140de76c2b5cf3992def43de3b0d4c0
Menfin <menfin.swgoh@gmail.com>
2018-07-25 13:26:30 +0200

    list le cache

--- toon.py
+++ toon.py
@@ -86,6 +86,7 @@ def generate_image(char, gear, stars, zetas_count):
 
 
 def get_image(char, gear, stars, zetas_count):
+    print(os.listdir("cache"))
     cache_name = "cache/" + char + "_g" + str(gear) + "_" + str(stars) + "stars_" + str(zetas_count) + "zetas.png"
     try:
         result = Image.open(cache_name)
